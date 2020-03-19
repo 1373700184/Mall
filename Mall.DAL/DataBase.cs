cde9866484c3070c4bc0f5f20416bbb0a30269b9
1373700184 <1373700184@qq.com>
2020-03-19 11:09:13 +0800

    9999

---
+++ Mall.DAL/DataBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Mall.Models;
+
+namespace Mall.DAL
+{
+    public class DataBase:DbContext
+    {
+        public DataBase() : base("DbMall")
+        { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("Users");
+        }
+        public DbSet<Users> UsersDAL { get; set; }
+    }
+}
