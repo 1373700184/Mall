c80f96f3dcec05ef712b798999155e8d96aa1060
1373700184 <1373700184@qq.com>
2020-03-19 15:39:49 +0800

    9999

---
+++ Mall.DAL/SQLBasis.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mall.DAL
+{
+    public class SQLBasis
+    {
+        private Type type;
+        private string TNamestr;
+        private string sql;
+        private string sqllast;
+
+        public SQLBasis(Type type)
+        {
+            this.type = type;
+            string Tname = type.Name;
+
+        }
+
+        public SQLBasis(string TNamestr)
+        {
+            this.TNamestr = TNamestr;
+        }
+
+
+        /// <summary>
+        /// 之间
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        public void Between(string colname, string v1, string v2)
+        {
+            string andsql = string.Format(@" and {0} between '{1}' and '{2}'", colname, v1, v2);
+            sql = sql + andsql;
+        }
+
+
+
+
+        /// <summary>
+        /// 等于
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        public void And(string colname, string v1)
+        {
+            string andsql = string.Format(@"and {0}={1}", colname, v1);
+            sql = sql + andsql;
+        }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <param name="v1">字段名</param>
+        /// <param name="v2">asc，desc</param>
+        public void Orderby(string v1, string v2)
+        {
+            sqllast = string.Format(@"Order by {0} {1}", v1, v2);
+        }
+
+
+        /// <summary>
+        /// 模糊查询
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        public void Like(string colname, string v1)
+        {
+            string andsql = string.Format(@"and {0} like '%{1}%'", colname, v1);
+            sql = sql + andsql;
+        }
+
+        /// <summary>
+        /// 更小
+        /// </summary>
+        /// <param name="v1">model的属性 </param>
+        /// <param name="v2">对比的值</param>
+        public void Smaller(string colname, string v1)
+        {
+            string andsql = string.Format(@"and {0}<{1} ", colname, v1);
+        }
+
+
+        /// <summary>
+        /// 手写的sql语句
+        /// </summary>
+        /// <param name="sqlnext"></param>
+        public void Sql(string sqlnext)
+        {
+            sql = sql + sqlnext;
+        }
+
+
+        /// <summary>
+        /// 在某段时间内
+        /// </summary>
+        /// <param name="minColname"></param>
+        /// <param name="maxColname"></param>
+        /// <param name="minT"></param>
+        /// <param name="maxT"></param>
+        public void InTime(string minColname, string maxColname, string minT, string maxT)
+        {
+            string andsql = string.Format($"and (({minColname}<'{maxT}' and  {maxColname} >'{minT}') or  ({maxColname}<'{maxT}' and  {minColname} >'{minT}'))");
+            sql = sql + andsql;
+        }
+
+
+        /// <summary>
+        /// 更大
+        /// </summary>
+        /// <param name="v1">model的属性 </param>
+        /// <param name="v2">对比的值</param>
+        public void Bigger(string colname, string v1)
+        {
+            string andsql = string.Format(@"and {0}<{1}", colname, v1);
+            sql = sql + andsql;
+        }
+
+        /// <summary>
+        /// 返回条件语句
+        /// </summary>
+        /// <returns></returns>
+        public string Sqllinq()
+        {
+
+            return sql + sqllast;
+        }
+
+
+
+        public string Tname()
+        {
+            if (!string.IsNullOrWhiteSpace(TNamestr))
+            {
+                return TNamestr;
+            }
+            return type.Name;
+        }
+    }
+}
---
+++ Mall.DAL/SQLExecute.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mall.DAL
+{
+    public class SQLExecute
+    {
+        DataBase zJDAL = new DataBase();
+        public static int Count<T>(T t)
+        {
+            Type type = typeof(T);           
+            SQLBasis sqlBasis = new SQLBasis("");
+            sqlBasis.And("", "");
+
+            return 1;
+        }
+        public void EG()
+        {
+            using (DataBase db = new DataBase())
+            {
+                var _param = new List<object>();                                    //定义ExecuteSqlCommand参数
+                _param.Add(1);                                                      //给in条件前面的参数赋值，根据实际情况
+                var _in = new object[] { "test@qq.com", "a@gmail.com", "b@163.com" };//in条件
+                _param.AddRange(_in);                                               //给in参数赋值
+                var sql = "UPDATE users SET islocked={0} WHERE email IN ({1})";
+                // .Replace("{1}", _in.Select((v, i) => "{" + (i + 1) + "}").Join(","));//给in条件的增加占位符,{"+(i+1)+"}中的1根据in条件前面有几个占位符而定
+                db.Database.ExecuteSqlCommand(sql, _param.ToArray());                    //执行
+            }
+        }
+
+
+        /// <summary>
+        /// 查找Model 的表
+        /// </summary>
+        /// <typeparam name="T">Model 名</typeparam>
+        /// <param name="sqlBasis"></param>
+        /// <returns></returns>
+
+        public List<T> GetAllModel<T>(SQLBasis sqlBasis)
+        {
+            string sql = sqlBasis.Sqllinq();
+
+            string type = sqlBasis.Tname();
+
+            string sqlwhere = string.Format(@"select * from {0} where 1=1 ", type);
+
+            sqlwhere = sqlwhere + sql;
+
+            return zJDAL.Database.SqlQuery<T>(sqlwhere).ToList();
+        }
+
+        /// <summary>
+        /// 查找一个model
+        /// </summary>
+        /// <typeparam name="T">Model 名</typeparam>
+        /// <param name="sqlBasis"></param>
+        /// <returns></returns>
+        public T GetOneModel<T>(SQLBasis sqlBasis)
+        {
+            string sql = sqlBasis.Sqllinq();
+
+            string type = sqlBasis.Tname();
+
+            string sqlwhere = string.Format(@"select * from {0} where 1=1 ", type);
+
+            sqlwhere = sqlwhere + sql;
+
+            return zJDAL.Database.SqlQuery<T>(sqlwhere).FirstOrDefault();
+        }
+
+        public T OneModel<T>(SQLBasis sqlBasis)
+        {
+            string sql = sqlBasis.Sqllinq();
+
+            string type = sqlBasis.Tname();
+
+            string sqlwhere = string.Format(@"select * from {0} where 1=1 ", type);
+
+            sqlwhere = sqlwhere + sql;
+
+            return zJDAL.Database.SqlQuery<T>(sqlwhere).FirstOrDefault();
+        }
+        /// <summary>
+        /// 总数
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sqlBasis"></param>
+        /// <returns></returns>
+        public int GetCount<T>(SQLBasis sqlBasis)
+        {
+            string sql = sqlBasis.Sqllinq();
+
+            string type = sqlBasis.Tname();
+
+            string sqlwhere = string.Format(@"select Count(*) from {0} where 1=1 ", type);
+
+            sqlwhere = sqlwhere + sql;
+
+            int i = 0;
+            using (DataBase DB = new DataBase())
+            {
+
+                i = Convert.ToInt16(DB.Database.SqlQuery<T>(sqlwhere));
+            }
+            return i;
+        }
+    }
+}
